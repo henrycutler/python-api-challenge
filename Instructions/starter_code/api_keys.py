d631f85a3fd13b4f18489b3fd0f811b5681aa125
Henry Cutler <henry@MacBook-Air.local>
2022-08-02 16:34:29 -0500

    final commit

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "25b5d19ab81867b225e0b82050a8eea1"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBgaCLPnbj8j_e4NyegweXWlRl_H0OY0dw"
